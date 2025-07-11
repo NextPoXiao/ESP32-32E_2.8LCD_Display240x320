7681e2f1e1489ca13f858e782526d309ad05ae4a
poxiao <useremail@qq.com>
2025-07-11 19:54:22 +0800

    chore(denky32): 更新 lvgl配置

--- src/main.cpp
+++ src/main.cpp
@@ -1,134 +1,143 @@
-/*
- Font draw speed and flicker test, draws all numbers 0-999 in each font
- Average time in milliseconds to draw is shown in red
- A total of 2890 characters are drawn in each font
- 
- Needs fonts 2, 4, 6, 7 and 8
+/*Using LVGL with Arduino requires some extra steps:
+ *Be sure to read the docs here: https://docs.lvgl.io/master/integration/framework/arduino.html  */
 
- Make sure all the display driver and pin connections are correct by
- editing the User_Setup.h file in the TFT_eSPI library folder.
+#include <lvgl.h>
 
- #########################################################################
- ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
- #########################################################################
- */
+#if LV_USE_TFT_ESPI
+#include <TFT_eSPI.h>
+#endif
 
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ *Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ *as the examples and demos are now part of the main LVGL library. */
 
-#include <TFT_eSPI.h> // Hardware-specific library
-#include <SPI.h>
+//#include <examples/lv_examples.h>
+//#include <demos/lv_demos.h>
 
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+/*Set to your screen resolution and rotation*/
+#define TFT_HOR_RES   240
+#define TFT_VER_RES   320
+#define TFT_ROTATION  LV_DISPLAY_ROTATION_0
 
-unsigned long drawTime = 0;
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
-void setup(void) {
-  tft.init();
-  tft.setRotation(1);
+#if LV_USE_LOG != 0
+void my_print( lv_log_level_t level, const char * buf )
+{
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
+}
+#endif
+
+/* LVGL calls it when a rendered image needs to copied to the display*/
+void my_disp_flush( lv_display_t *disp, const lv_area_t *area, uint8_t * px_map)
+{
+    /*Copy `px map` to the `area`*/
+
+    /*For example ("my_..." functions needs to be implemented by you)
+    uint32_t w = lv_area_get_width(area);
+    uint32_t h = lv_area_get_height(area);
+
+    my_set_window(area->x1, area->y1, w, h);
+    my_draw_bitmaps(px_map, w * h);
+     */
+
+    /*Call it to tell LVGL you are ready*/
+    lv_display_flush_ready(disp);
 }
 
-void loop() {
+/*Read the touchpad*/
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
+{
+    /*For example  ("my_..." functions needs to be implemented by you)
+    int32_t x, y;
+    bool touched = my_get_touch( &x, &y );
 
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    if(!touched) {
+        data->state = LV_INDEV_STATE_RELEASED;
+    } else {
+        data->state = LV_INDEV_STATE_PRESSED;
 
-  drawTime = millis();
+        data->point.x = x;
+        data->point.y = y;
+    }
+     */
+}
 
-  for (int i = 0; i < 1000; i++) {
-    yield();tft.drawNumber(i, 100, 80, 1);
-  }
+/*use Arduinos millis() as tick source*/
+static uint32_t my_tick(void)
+{
+    return millis();
+}
 
-  drawTime = millis() - drawTime;
+void setup()
+{
+    String LVGL_Arduino = "Hello Arduino! ";
+    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
 
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  int xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 100) tft.drawString("Font 1 not loaded!", 50, 210, 4);
-  
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    drawTime = millis();
+    Serial.begin( 115200 );
+    Serial.println( LVGL_Arduino );
 
-  for (int i = 0; i < 1000; i++) {
-    yield();tft.drawNumber(i, 100, 80, 2);
-  }
+    lv_init();
 
-  drawTime = millis() - drawTime;
+    /*Set a tick source so that LVGL will know how much time elapsed. */
+    lv_tick_set_cb(my_tick);
 
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200) tft.drawString("Font 2 not loaded!", 50, 210, 4);
-  
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    drawTime = millis();
+    /* register print function for debugging */
+#if LV_USE_LOG != 0
+    lv_log_register_print_cb( my_print );
+#endif
 
-  for (int i = 0; i < 1000; i++) {
-    yield();tft.drawNumber(i, 100, 80, 4);
-  }
+    lv_display_t * disp;
+#if LV_USE_TFT_ESPI
+    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+    lv_display_set_rotation(disp, TFT_ROTATION);
 
-  drawTime = millis() - drawTime;
+#else
+    /*Else create a display yourself*/
+    disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+    lv_display_set_flush_cb(disp, my_disp_flush);
+    lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+#endif
 
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200) tft.drawString("Font 4 not loaded!", 50, 210, 4);
-  
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    drawTime = millis();
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t * indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
 
-  for (int i = 0; i < 1000; i++) {
-    yield();tft.drawNumber(i, 100, 80, 6);
-  }
+    /* Create a simple label
+     * ---------------------
+     lv_obj_t *label = lv_label_create( lv_screen_active() );
+     lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
 
-  drawTime = millis() - drawTime;
+     * Try an example. See all the examples
+     *  - Online: https://docs.lvgl.io/master/examples.html
+     *  - Source codes: https://github.com/lvgl/lvgl/tree/master/examples
+     * ----------------------------------------------------------------
 
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200) tft.drawString("Font 6 not loaded!", 50, 210, 4);
-  
-  delay(4000);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    drawTime = millis();
+     lv_example_btn_1();
 
-  for (int i = 0; i < 1000; i++) {
-    yield();tft.drawNumber(i, 100, 80, 7);
-  }
+     * Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMO_WIDGETS
+     * -------------------------------------------------------------------------------------------
 
-  drawTime = millis() - drawTime;
+     lv_demo_widgets();
+     */
 
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200) tft.drawString("Font 7 not loaded!", 50, 210, 4);
-  
-  delay(4000);
-  tft.fillScreen(TFT_YELLOW);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    drawTime = millis();
+    lv_obj_t *label = lv_label_create( lv_screen_active() );
+    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
 
-  for (int i = 0; i < 1000; i++) {
-    yield();tft.drawNumber(i, 100, 80, 8);
-  }
+    Serial.println( "Setup done" );
+}
 
-  drawTime = millis() - drawTime;
-
-  tft.setTextColor(TFT_RED, TFT_BLACK);
-  xpos = 20;
-  xpos += tft.drawFloat(drawTime / 2890.0, 3, xpos, 180, 4);
-  tft.drawString(" ms per character", xpos, 180, 4);
-  if (drawTime < 200) tft.drawString("Font 8 not loaded!", 50, 210, 4);
-  
-  delay(4000);
+void loop()
+{
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5); /* let this time pass */
 }
